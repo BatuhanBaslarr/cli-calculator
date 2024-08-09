feb989ee6c12edd827b93369a2bebc20778ef1f7
BatuhanBaslar <batuhanbaslar@gmail.com>
2024-08-09 18:14:44 +0300

    .gitignore works now

--- calculator.go
+++ calculator.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 // Main Function
 func main() {
-	application()
+	type1 := flag.Bool("type1", false, "Basic Calculator")
+	type2 := flag.Bool("type2", false, "Scientific Calculator")
+
+	flag.Parse()
+
+	if *type1 {
+		bscCalc()
+
+	} else if *type2 {
+
+		sciCalc()
+
+	} else {
+		fmt.Println("Invalid operation. Use -type1 for basic or -type2 for scientific calculator.")
+	}
+
 }
